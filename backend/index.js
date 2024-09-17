267265e89178f993a73a1cbc616e052894cc0125
TaniaMAHOUCHE <mahouchetania15@gmail.com>
2024-09-17 17:08:33 +0200

    creation models, routes et controllers activity et address

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,24 @@
-// index.js
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const activityRoutes = require('./routes/ActivityRoutes'); 
+const addressRoutes = require('./routes/AddressRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/kbo_db');
+// mongoose.connect('mongodb://localhost:27017/kbo_db');
+mongoose.connect('mongodb://127.0.0.1:27017/kbo_db');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
+
+app.use('/activities', activityRoutes);
+app.use('/addresses', addressRoutes);
 
 app.use(bodyParser.json());
 app.use(cors());
---
+++ backend/models/Address.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const addressSchema = new mongoose.Schema({
+  EntityNumber: {
+    type: String,
+    required: true
+  },
+  TypeOfAddress: {
+    type: String,
+    required: true
+  },
+  CountryNL: {
+    type: String
+  },
+  CountryFR: {
+    type: String
+  },
+  Zipcode: {
+    type: String,
+    required: true
+  },
+  MunicipalityNL: {
+    type: String,
+    required: true
+  },
+  MunicipalityFR: {
+    type: String,
+    required: true
+  },
+  StreetNL: {
+    type: String,
+    required: true
+  },
+  StreetFR: {
+    type: String,
+    required: true
+  },
+  HouseNumber: {
+    type: String,
+    required: true
+  },
+  Box: {
+    type: String
+  },
+  ExtraAddressInfo: {
+    type: String
+  },
+  DateStrikingOff: {
+    type: Date
+  }
+});
+
+module.exports = mongoose.model('Address', addressSchema);
