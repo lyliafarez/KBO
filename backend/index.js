bdae95873536bc79a5fc9abf97c2481bc19f47a5
lyliafarez <lylia@heliop.com>
2024-09-17 15:17:58 +0200

    main / initial commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+// index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost:27017/kbo_db');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
