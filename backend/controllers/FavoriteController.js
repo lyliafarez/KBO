1a0ae09d17e5f4024a95b0b60bc475f079c4f015
TaniaMAHOUCHE <mahouchetania15@gmail.com>
2024-09-20 14:47:29 +0200

    Update

--- backend/controllers/FavoriteController.js
+++ backend/controllers/FavoriteController.js
@@ -1,42 +1,9 @@
 const Favorite = require('../models/Favorite');
 
 // Créer un favori
-// exports.createFavorite = async (req, res) => {
-//   const { idUser, idEntreprise } = req.body;
-
-//   // if (!idUser || !idEntreprise) {
-//   //   return res.status(400).json({ message: 'idUser et idEntreprise sont requis' });
-//   // }
-
-//   if (!idEntreprise) {
-//     return res.status(400).json({ message: 'L\'identifiant de l\'entreprise (idEntreprise) est requis.' });
-//   }
-
-//   if (!idUser) {
-//     return res.status(400).json({ message: 'L\'identifiant de l\'utilisateur (idUser) est requis.' });
-//   }
-
-//   // if (!idEntreprise) {
-//   //   return res.status(400).json({ message: 'L\'identifiant de l\'entreprise (idEntreprise) est requis.' });
-//   // }
-
-//   try {
-//     const newFavorite = new Favorite({ idUser, idEntreprise });
-//     const savedFavorite = await newFavorite.save();
-//     res.status(201).json(savedFavorite);
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// };
-
-// Créer un favori
 exports.createFavorite = async (req, res) => {
   const { idUser, idEntreprise } = req.body;
   console.log('Received body:', req.body); // Affiche le corps de la requête
-
-  // if (!idEntreprise || !idUser) {
-  //   return res.status(400).json({ message: 'L\'identifiant de l\'entreprise (idEntreprise) et l\'identifiant de l\'utilisateur (idUser) sont requis.' });
-  // }
 
   if (!idEntreprise) {
     return res.status(400).json({ message: 'L\'identifiant de l\'entreprise (idEntreprise) est requis.' });
@@ -45,14 +12,6 @@ exports.createFavorite = async (req, res) => {
   if (!idUser) {
     return res.status(400).json({ message: 'L\'identifiant de l\'utilisateur (idUser) est requis.' });
   }
-
-  // try {
-  //   const newFavorite = new Favorite({ idUser, idEntreprise });
-  //   const savedFavorite = await newFavorite.save();
-  //   res.status(201).json(savedFavorite);
-  // } catch (error) {
-  //   res.status(400).json({ message: error.message });
-  // }
 
   try {
     const favorite = new Favorite({ idUser, idEntreprise });
@@ -115,24 +74,6 @@ exports.updateFavorite = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
-
-// Supprimer un favori
-// exports.deleteFavorite = async (req, res) => {
-//   const { id } = req.params;
-
-//   try {
-//     const deletedFavorite = await Favorite.findByIdAndDelete(id);
-
-//     if (!deletedFavorite) {
-//       return res.status(404).json({ message: 'Favori non trouvé' });
-//     }
-
-//     res.status(200).json({ message: 'Favori supprimé avec succès' });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
 
 exports.deleteFavorite = async (req, res) => {
   const { id } = req.params;
